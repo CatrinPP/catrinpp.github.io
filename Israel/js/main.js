d6fb3fc8c016916167101c02f24bead734c707c8
CatrinPP <caterina.parshina@gmail.com>
2020-06-15 16:58:50 +0300

    доработки israel

--- Israel/js/main.js
+++ Israel/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+
+  /* Открытие и закрытие модальных окон */
   var ESC_KEYCODE = 27;
 
   var callPopup = document.querySelector('.modal--callback');
@@ -20,6 +22,7 @@
   var closePopup = function (popup) {
     popup.classList.remove('modal--show');
     document.removeEventListener('keydown', onEscPress);
+    document.body.style.overflow = 'visible';
   };
 
   var closeAnyActivePopup = function () {
@@ -46,21 +49,25 @@
   };
 
   var onSubmit = function (evt) {
+    localStorage.setItem('userName', evt.target.querySelector('#user-name').value);
+    localStorage.setItem('userTel', evt.target.querySelector('#user-tel').value);
     evt.preventDefault();
     closeAnyActivePopup();
     openSuccessPopup();
   };
 
-  var onCallButtonClick = function () {
+  var onCallButtonClick = function (evt) {
+    evt.preventDefault();
     callPopup.classList.add('modal--show');
+    document.body.style.overflow = 'hidden';
     callPopup.querySelector('input[type="text"]').value = '';
     callPopup.querySelector('input[type="tel"]').value = '';
     callPopup.querySelector('input[type="checkbox"]').checked = false;
     callPopupCloseButton.addEventListener('click', function () {
       closePopup(callPopup);
     });
-    callPopup.addEventListener('click', function (evt) {
-      if (evt.target === callPopup) {
+    callPopup.addEventListener('click', function (e) {
+      if (e.target === callPopup) {
         closePopup(callPopup);
       }
     });
@@ -69,4 +76,52 @@
 
   document.addEventListener('submit', onSubmit);
   callButton.addEventListener('click', onCallButtonClick);
+
+  /* Маска для ввода телефона */
+  var input = document.querySelector('#user-tel');
+
+  var setCursorPosition = function (position, element) {
+    element.focus();
+    if (element.setSelectionRange) {
+      element.setSelectionRange(position, position);
+    } else if (element.createTextRange) {
+      var range = element.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', position);
+      range.moveStart('character', position);
+      range.select();
+    }
+  };
+
+  var mask = function (event) {
+    var matrix = '+7 (___) ___ __ __';
+    var index = 0;
+    var newValue = matrix.replace(/\D/g, '');
+    var value = input.value.replace(/\D/g, '');
+
+    if (newValue.length >= value.length) {
+      value = newValue;
+    }
+
+    input.value = matrix.replace(/./g, function (char) {
+      if (/[_\d]/.test(char) && index < value.length) {
+        return value.charAt(index++);
+      } else if (index >= value.length) {
+        return '';
+      }
+      return char;
+    });
+    if (event.type === 'blur') {
+      if (input.value.length === 2) {
+        input.value = '';
+      }
+    } else {
+      setCursorPosition(input.value.length, input);
+    }
+  };
+
+  input.addEventListener('input', mask, false);
+  input.addEventListener('focus', mask, false);
+  input.addEventListener('blur', mask, false);
+
 })();
