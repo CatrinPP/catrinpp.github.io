f912728ef58a10624a0b4dc2adffc708150f9f1f
CatrinPP <caterina.parshina@gmail.com>
2020-06-23 13:22:02 +0300

    israel header test

--- Israel/js/main.js
+++ Israel/js/main.js
@@ -139,7 +139,7 @@
     }
   };
 
-  var mask = function (event) {
+  var changeTelephoneInputValue = function (event) {
     var input = event.target;
     var matrix = '+7 (___) ___ __ __';
     var index = 0;
@@ -169,9 +169,9 @@
 
   function setInputsListeners(array) {
     for (var i = 0; i < array.length; i++) {
-      array[i].addEventListener('input', mask, false);
-      array[i].addEventListener('focus', mask, false);
-      array[i].addEventListener('blur', mask, false);
+      array[i].addEventListener('input', changeTelephoneInputValue, false);
+      array[i].addEventListener('focus', changeTelephoneInputValue, false);
+      array[i].addEventListener('blur', changeTelephoneInputValue, false);
     }
   }
 
@@ -347,4 +347,17 @@
 
   setPreviousButtonsListeners();
   setNextButtonsListeners();
+
+  /* Якорный скролл */
+  var scroll = document.querySelector('.header__scroll');
+
+  scroll.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    var id = scroll.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
 })();
